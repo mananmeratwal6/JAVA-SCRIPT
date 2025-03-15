045044afca6fbfbf49036e264fda332baa0f21a7
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-03-15 12:31:38 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)we are using nodejs,not browser
+
+console.log(3+3);
+console.log("Manan")
+
+let name = "manan"  //string
+let age = 18 //int
+let isLoggedIn = false //Bollean
+
+// number =>2 to powwer 53
+// BigInt
+// // string =>""
+// Boolean => true/false
+//null => standalone value /type object
+//undefined =>
+// symbol=>unique
+
+//object
+
+console.log(typeof undefined)

7ff75ed1b41e79fa43ae27339d61251ebcb980a8
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-03-15 19:41:56 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("Manna");
+
+// start conversion form here
+console.log("2" > 1);
+console.log("02" > 1);
+
+// we dont know where it convert it into null or zero
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  => it check datatype also
+console.log("2"=== 2);
+console.log("2"==2);

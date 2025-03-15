33e9226c0ff2ef2d3a3fb83661dd766d2c458922
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-03-15 18:13:51 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,73 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************Operation***********
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);
+
+
+let str1 = "Mana"
+let str2 = "n"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3,num4
+num1 = num2 = num3 =num4=3+3
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+
+ let x = 3;
+ const y = x++;
+ 
+ console.log(`x:${x}, y:${y}`);
+ // Expected output: "x:4, y:3"
+ 
+ let a = 3;
+ const b = ++a;
+ 
+ console.log(`a:${a}, b:${b}`);
+ // Expected output: "a:4, b:4"
+ 

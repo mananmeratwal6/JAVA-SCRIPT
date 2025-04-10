09cc820a644855992012e5f42442590e7dfbd260
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-04-10 13:35:09 +0000

    objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -17,3 +17,39 @@ const regularUser = {
     }
 }
 console.log(regularUser.fullname.userfullname)
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj3 = {5:"a",6:"b"}
+
+// const obj4 = {obj1, obj2}//wrong approach
+
+// const obj4 = Object.assign({},obj1,obj2,obj3) target and source 
+const obj4 = {...obj1,...obj2}
+console.log(obj4);
+
+const users = [
+    {
+        id : 1,
+        email:"Man@gmail.com"
+    },
+    {
+        id : 1,
+        email:"Man@gmail.com"
+    },
+    {
+        id : 1,
+        email:"Man@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));// as it returns array so it is very usefull to use in database and other things
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));//convert all keys and values in array into array
+console.log(tinderUser.hasOwnProperty('isLogedIn') );
+
+
+
+

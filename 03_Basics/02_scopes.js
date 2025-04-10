7baf544140f04e2aed668d5324e094463d756849
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-04-10 18:24:17 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,11 @@
+let a
+if(true){
+    let a =10;
+    const b =20
+    // var c=30
+    console.log("Inner ",a)
+    
+}
+// console.log(a);
+// console.log(b);//both scope is in {} braces we can use global values in {} but values in {} cant use in outside until return
+console.log(c);

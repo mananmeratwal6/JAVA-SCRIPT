7f2e7d64f2850bba3caef6138755dd61bb6fbd3c
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-04-12 17:47:41 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+ if(true){//if conditon true then execute else not execute
+
+ }
+
+ const isuserloggedIn = true
+ if(isuserloggedIn){
+
+ }
+ if(2=="2"){
+    console.log("executed");//executed
+    
+ }
+ if(2==="2"){
+    console.log("executed");// not executed
+    
+ }
+ if(2!=="2"){
+    console.log("executed");// executed
+    
+ }
+ const temp=41
+ if(temp===41){
+    console.log("less than 50");
+    
+ }
+ else{
+    console.log("greater than 50");
+ }
+ const balance =1000
+ if(balance>500)
+    console.log("test"),
+   console.log("test2")
+   ;
+
+   // if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+    
